fcfd9b87361cf434b0ea03be0062e35307746d97
Kongxib Vue <kongcvue@gmail.com>
2021-10-10 13:54:53 -0500

    page is setup

---
+++ server/public/scripts/client.js
@@ -0,0 +1,13 @@
+$(onReady);
+
+function onReady(){
+    console.log('jquery loaded!');
+
+    //click listeners\
+    $(`.addButton`).on(`click`, add)
+    $(`.subtractButton`).on(`click`, subtract)
+    $(`.subtractButton`).on(`click`, mutliply)
+    $(`.subtractButton`).on(`click`, )
+
+
+}
