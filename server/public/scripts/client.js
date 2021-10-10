ab211bf7298b12c4899d82739bcb11308c796582
Kongxib Vue <kongcvue@gmail.com>
2021-10-10 16:21:49 -0500

    operator functions done

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,8 +6,74 @@ function onReady(){
     //click listeners\
     $(`.addButton`).on(`click`, add)
     $(`.subtractButton`).on(`click`, subtract)
-    $(`.subtractButton`).on(`click`, mutliply)
-    $(`.subtractButton`).on(`click`, )
+    $(`.multiplyButton`).on(`click`, multiply)
+    $(`.divideButton`).on(`click`, divide)
+    $(`.equalsButton`).on(`click`, equals)
+    $(`.clearButton`).on(`click`, clear)
 
-
+    getCalculations();
 }
+    function equals(){
+        $.ajax({
+            method: 'POST',
+            url: '/calculate',
+            data:{
+                firstNum: $(`#firstNum`).val(),
+                lastNum: $(`#lastNum`).val(),
+                op: $(`.equalsButton`).data("op")
+            }
+        }).then(function(response){
+            console.log('This is the ');
+        }).catch(function(response){
+            console.log('Request Failed');
+        })
+    }
+
+
+    function getCalculations(){
+        //get calculations from the server
+        $.ajax({
+            method: 'GET',
+            url: '/results'
+        }).then(function(response){
+            console.log('this is the calculations array', response);
+            renderToDom(response);
+        }).catch(function(response){
+            console.log('Request failed');
+        })
+    }
+
+    function clear(){
+        $.ajax({
+            method: 'GET',
+            url: '/clear'
+        }).then(function(response){
+            console.log('this is the calculations array', response);
+            renderToDom(response);
+        }).catch(function(response){
+            console.log('Request failed');
+        })
+    }
+
+    function add(){
+    $(`.equalsButton`).data("op", "+");
+    }
+    function subtract(){
+    $(`.equalsButton`).data("op", "-");
+    }
+    function multiply(){
+    $(`.equalsButton`).data("op", "*");
+    }
+    function divide(){
+    $(`.equalsButton`).data("op", "/");
+    }
+
+    function renderToDom(array){
+        $(`.total`).empty();
+        for(let index of array){
+            console.log('render to DOM the results', array);
+            if 
+        }
+    }
+
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 5000;
+let resultsArray = [];
 
 //this needs to be added for GET and POST routes
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -12,3 +13,26 @@ app.use(express.static("server/public"));
 app.listen(PORT, () =>{
     console.log("Server is running on port", PORT);
 });
+
+
+app.post("/calculate", (req,res) => {
+    console.log('This is the req.body ', req.body);
+    //grabs the input values and equalsButton op value
+    let equation = req.body;
+
+    console.log('this is the equation data!', equation);
+
+    calculate
+})
+
+app.get("/results", (req, res) => {
+    console.log('This is the getCalculations button');
+    res.send(resultsArray);
+})
+
+app.get("/clear", (req, res) =>{
+    console.log("This is the clear button GET");
+    resultsArray = [];
+    res.send(resultsArray);
+})
+
